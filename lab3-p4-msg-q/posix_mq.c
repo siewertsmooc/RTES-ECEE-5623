50fca1fa9ba5c58f7fc95221cbae77742b062f3a
Lorin A <acheylorin@gmail.com>
2022-07-05 14:39:33 -0600

    Fixed compiler errors

--- lab3-p4-msg-q/posix_mq.c
+++ lab3-p4-msg-q/posix_mq.c
@@ -43,7 +43,7 @@ struct sched_param param_send;
 
 static char canned_msg[] = "this is a test, and only a test, in the event of a real emergency, you would be instructed ...";
 
-void receiver(void)
+void *receiver(void)
 {
   mqd_t mymq;
   char buffer[MAX_MSG_SIZE];
@@ -73,7 +73,7 @@ void receiver(void)
 }
 
 
-void sender(void)
+void *sender(void)
 {
   mqd_t mymq;
   int prio;
@@ -154,8 +154,8 @@ void main(void)
   }
 
   printf("pthread join send\n");  
-  pthread_join(th_send, NULL);
+  pthread_join(thread_send, NULL);
 
   printf("pthread join receive\n");  
-  pthread_join(th_receive, NULL);
+  pthread_join(thread_receive, NULL);
 }
