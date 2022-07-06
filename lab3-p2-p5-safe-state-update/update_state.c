6fb1bcb1db0ef082c9a6dea46d652d94952414bf
Lorin <acheylorin@gmail.com>
2022-07-05 21:07:12 -0600

    Add the initial updater method

--- lab3-p2-p5-safe-state-update/update_state.c
+++ lab3-p2-p5-safe-state-update/update_state.c
@@ -16,7 +16,37 @@ typedef struct {
    double longitude;
    double altitude;
    struct timespec timestamp;
-} position_state_t
+} position_state_t;
 
+position_state_t state;
 
+pthread_t stateUpdaterThread;
+pthread_t stateReaderThread;
 
+pthread_mutex_t state_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+double timestamp;
+
+void *updateState() {
+
+   pthread_mutex_lock(&state_mutex);
+
+   // Update the state values and timestamp
+   state.roll = ((double) rand());
+   state.pitch = ((double) rand());
+   state.yaw = ((double) rand());
+   state.latitude = ((double) rand());
+   state.longitude = ((double) rand());
+   state.altitude = ((double) rand());
+
+	clock_gettime(CLOCK_REALTIME,&(state.timestamp));
+	timestamp = (double)state.timestamp.tv_sec;
+	timestamp += (double)state.timestamp.tv_nsec/(double)1000000000;
+   pthread_mutex_unlock(&state_mutex);
+
+}
+
+int main(int argc, char *argv[])
+{
+   srand( time(NULL) );
+}
