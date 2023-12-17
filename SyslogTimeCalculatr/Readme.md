9be98dadd202db817618df6708b5a4f675827d03
A.Amani <didi1364@gmail.com>
2023-12-17 15:27:03 +0400

    feat: add new tool to calculate syslog time-stamps

---
+++ SyslogTimeCalculatr/Readme.md
@@ -0,0 +1,5 @@
+
+This tool extracts text from syslog and calculates the period (T), frequency, maximum (WCET), minimum, average, and standard deviations for each service. 
+A sample syslog entry should resemble the following:
+Dec 17 13:07:42 raspberrypi sequencer[27447]: S1 50 Hz on core 2 for release 11 @ sec=0.229461354
+Please note that the provided syslog entry is tailored to the required format. Adjust the actual syslog entries accordingly for accurate processing by the tool.
---
+++ SyslogTimeCalculatr/src/mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QDebug>
+#include <cmath>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QString calc(QList<double> timeStamps)
+{
+
+    double sum = std::accumulate(timeStamps.constBegin(), timeStamps.constEnd(), 0.0);
+    double average = sum / timeStamps.size();
+
+    // Calculate maximum and minimum
+    double maximum = *std::max_element(timeStamps.constBegin(), timeStamps.constEnd());
+    double minimum = *std::min_element(timeStamps.constBegin(), timeStamps.constEnd());
+
+    // Calculate standard deviation
+    double sumSquares = 0.0;
+    for (double timestamp : timeStamps) {
+        sumSquares += std::pow(timestamp - average, 2);
+    }
+    double variance = sumSquares / timeStamps.size();
+    double standardDeviation = std::sqrt(variance);
+    return  "AVG="+QString::number(average)+" std="+QString::number(standardDeviation)+
+            " min="+QString::number(minimum)+" max="+QString::number(maximum)+" count="+QString::number(timeStamps.length());
+
+
+}
+void MainWindow::on_BtnCalc_clicked()
+{
+    QString input=ui->TxtInput->toPlainText();
+    QList<double> timeStamps;
+
+    QStringList lines=input.split("\n");
+    for(int i=1;i<lines.count();i++)
+    {
+        if(lines[i].contains(ui->TxtServiceName->text()) && lines[i].contains("sec="))
+        {
+
+            timeStamps.push_back(lines[i].split("sec=")[1].toDouble()-lines[i-1].split("sec=")[1].toDouble());
+        }
+
+    }
+
+    ui->TxtResults->setText(calc(timeStamps));
+
+
+}
