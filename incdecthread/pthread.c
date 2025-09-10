d181b9043f3b56a4a84a25b2c09bb21b487000e8
Sam Siewert <56521981+siewertsmooc@users.noreply.github.com>
2025-09-09 18:44:27 -0700

    improved atomic
    improved atomic

--- incdecthread/pthread.c
+++ incdecthread/pthread.c
@@ -5,7 +5,13 @@
 
 #include <stdatomic.h>
 
-#define COUNT  1000
+// If you test at more than 1 billion and/or with a larger INCDEC_AMOUNT, you
+// risk an overflow.
+//
+#define COUNT  (1000000)
+//#define COUNT  (1000000000)
+
+#define INCDEC_AMOUNT (1)
 
 typedef struct
 {
@@ -19,10 +25,22 @@ pthread_t threads[2];
 threadParams_t threadParams[2];
 
 
+// In general I have noticed higher liklihood of corruption due to read-modify-write race
+// conditions on some machines (e.g., NUC cluster nodes) compared to others (e.g., ECC-Linux and
+// much more so if you add a larger number than incrementing or decrementing by just 1.
+//
+// Likewise lots of standard output can slow this down and impact results.
+//
+// Try enabling and disabling tracing I/O to see the impact.
+
 // Unsafe global
 int gsum=0;
 
-// Safer ATOMIC global
+// Safer ATOMIC global for C-11 compilers
+//
+// https://pubs.opengroup.org/onlinepubs/9799919799/basedefs/stdatomic.h.html
+//
+//_Atomic int agsum=0;
 atomic_int agsum=0;
 
 void *incThread(void *threadp)
@@ -32,9 +50,12 @@ void *incThread(void *threadp)
 
     for(i=0; i<COUNT; i++)
     {
-        gsum=gsum+i;
-        agsum=agsum+i;
-        printf("Increment thread idx=%d, gsum=%d,  agsum=%d\n", threadParams->threadIdx, gsum, agsum);
+        gsum=gsum+INCDEC_AMOUNT;
+
+        atomic_fetch_add(&agsum, INCDEC_AMOUNT);
+        //agsum=agsum+INCDEC_AMOUNT;
+
+        //printf("Increment thread idx=%d, gsum=%d,  agsum=%d\n", threadParams->threadIdx, gsum, agsum);
     }
     return (void *)0;
 }
@@ -47,9 +68,12 @@ void *decThread(void *threadp)
 
     for(i=0; i<COUNT; i++)
     {
-        gsum=gsum-i;
-        agsum=agsum-i;
-        printf("Decrement thread idx=%d, gsum=%d, agsum=%d\n", threadParams->threadIdx, gsum, agsum);
+        gsum=gsum-INCDEC_AMOUNT;
+
+        atomic_fetch_sub(&agsum, INCDEC_AMOUNT);
+        //agsum=agsum-INCDEC_AMOUNT;
+
+        //printf("Decrement thread idx=%d, gsum=%d, agsum=%d\n", threadParams->threadIdx, gsum, agsum);
     }
     return (void *)0;
 }
@@ -75,5 +99,6 @@ int main (int argc, char *argv[])
    for(i=0; i<2; i++)
      pthread_join(threads[i], NULL);
 
+   printf("Main thread gsum=%d,  agsum=%d\n", gsum, agsum);
    printf("TEST COMPLETE\n");
 }
