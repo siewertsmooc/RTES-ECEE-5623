a96f056e5bf46bfad0b996c6a6bcac71f3ea235e
Mason McGaffin <mamc3334@colorado.edu>
2026-06-23 23:48:53 -0600

    re-write simple mq demo with pthreads

--- POSIX-Examples-Updated/posix_mq.c
+++ POSIX-Examples-Updated/posix_mq.c
@@ -30,8 +30,16 @@
 
 struct mq_attr mq_attr;
 
-void receiver(void)
+pthread_t th_receive, th_send; // create threads
+pthread_attr_t attr_receive, attr_send;
+struct sched_param param_receive, param_send;
+
+static char canned_msg[] = "this is a test, and only a test, in the event of a real emergency, you would be instructed ...";
+
+void *receiver(void *arg)
 {
+  (void)arg;
+
   mqd_t mymq;
   char buffer[MAX_MSG_SIZE];
   unsigned int prio;
@@ -43,6 +51,10 @@ void receiver(void)
   {
     perror("receiver mq_open");
     exit(-1);
+  }
+  else
+  {
+    printf("receviver opened mq\n");
   }
 
   /* read oldest, highest priority msg from the message queue */
@@ -57,12 +69,13 @@ void receiver(void)
            buffer, prio, nbytes);
   }
     
+  return NULL;
 }
 
-static char canned_msg[] = "this is a test, and only a test, in the event of a real emergency, you would be instructed ...";
-
-void sender(void)
+void *sender(void *arg)
 {
+  (void)arg;
+
   mqd_t mymq;
   //unsigned int prio;
   int nbytes;
@@ -89,21 +102,67 @@ void sender(void)
     printf("send: message successfully sent\n");
   }
   
+  return NULL;
 }
-
 
 int main(void)
 {
+  int rc, rt_max_prio;
+
+  mq_unlink(SNDRCV_MQ);
 
   /* setup common message q attributes */
   mq_attr.mq_maxmsg = 10;
   mq_attr.mq_msgsize = MAX_MSG_SIZE;
-
   mq_attr.mq_flags = 0;
 
-
   // Create two communicating processes right here
-  sender();
-  receiver();
-   
+  // sender();
+  // receiver();
+
+  rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+  
+  // Setup Receiver
+  pthread_attr_init(&attr_receive);
+  pthread_attr_setinheritsched(&attr_receive, PTHREAD_EXPLICIT_SCHED);
+  pthread_attr_setschedpolicy(&attr_receive, SCHED_FIFO); 
+  param_receive.sched_priority = rt_max_prio-1;
+  pthread_attr_setschedparam(&attr_receive, &param_receive);
+  
+  // Setup Sender
+  pthread_attr_init(&attr_send);
+  pthread_attr_setinheritsched(&attr_send, PTHREAD_EXPLICIT_SCHED);
+  pthread_attr_setschedpolicy(&attr_send, SCHED_FIFO); 
+  param_send.sched_priority = rt_max_prio;
+  pthread_attr_setschedparam(&attr_send, &param_send);
+  
+  if((rc=pthread_create(&th_send, &attr_send, sender, NULL)) == 0)
+  {
+    printf("Sender Thread Created\n");
+  }
+  else 
+  {
+    perror("Failed to Make Sender Thread\n");
+    printf("rc=%d\n", rc);
+    return EXIT_FAILURE;
+  }
+
+  if((rc=pthread_create(&th_receive, &attr_receive, receiver, NULL)) == 0)
+  {
+    printf("Receiver Thread Created\n");
+  }
+  else
+  {
+    perror("Failed Making Reciever Thread\n"); 
+    printf("rc=%d\n", rc);
+    return EXIT_FAILURE;
+  }
+
+  pthread_join(th_send, NULL);
+  printf("Sender joined\n");  
+
+  pthread_join(th_receive, NULL);
+  printf("Reciever joined\n");
+
+  return EXIT_SUCCESS;
 }
